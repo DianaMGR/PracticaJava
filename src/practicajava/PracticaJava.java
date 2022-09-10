5466d2e0e66c256f57f6d23c92afc5f653eb6433
Diana Milena Gonzalez Rodriguez <dianagr2005@hotmail.com>
2022-09-10 10:49:11 -0500

    Iniciando proyecto de practica

---
+++ src/practicajava/PracticaJava.java
@@ -0,0 +1,15 @@
+package practicajava;
+
+/**
+ *
+ * @author DIANA
+ */
+public class PracticaJava {
+
+    public static void main(String[] args) {
+      
+        
+        System.out.println((10.5+2*3)/(45-3.5));
+    }
+    
+}
